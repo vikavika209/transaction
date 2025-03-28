9e7b5982e0eabdc9ea0d353d4646df705ac98e62
User2 <vkagarmanova@inbox.ru>
2025-03-28 16:55:04 +0500

    добавлена аннотация @ActiveProfiles("test")

--- transaction/src/test/java/com/example/transaction/controller/ClientControllerTest.java
+++ transaction/src/test/java/com/example/transaction/controller/ClientControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import java.math.BigDecimal;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class ClientControllerTest {
 
     private ExchangeRate exchangeRate;
